cce54044ecc6a06c9b2c66d27d3045faf6285520
Gabriela Medeiros da Silva <gabriela.medeiros2010@hotmail.com.br>
2021-09-16 06:47:45 -0300

    Removing tests and typecheck step

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import { API_URL } from '@env';
 
 const api = axios.create({
   // baseURL: API_URL,
-  baseURL: 'http://192.168.0.48:3000',
+  baseURL: 'http://192.168.0.120:3000',
   timeout: 10000,
   timeoutErrorMessage: 'Isso está demorando mais do que deveria, você pode tentar de novo?',
   headers: {
