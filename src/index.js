007df93b4b780afa970e518cf1b5c4af4ddee087
Gabriela Medeiros da Silva <gabriela.medeiros2010@hotmail.com.br>
2021-10-22 20:09:26 -0300

    Redirect to login on expiration  of token

--- src/index.js
+++ src/index.js
@@ -1,18 +1,40 @@
 import 'react-native-gesture-handler';
 
-import * as React from 'react';
+import React, { useEffect, useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 
 import { useApplicationProvider } from './providers/ApplicationProvider';
 import AuthRoutes from './routes/AuthRoutes';
 import AppRoutes from './routes/AppRoutes';
+import api from './services/api';
+import { Toast } from './components';
 
 const App = () => {
-  const { isSigned } = useApplicationProvider();
+  const { isSigned, signOut } = useApplicationProvider();
+  const [toastVisible, setToastVisible] = useState(false);
+
+  useEffect(() => {
+    api.interceptors.response.use(
+      function (response) {
+        return response;
+      },
+      function (error) {
+        if (error.response.status === 401) {
+          setToastVisible(true);
+          signOut();
+        }
+      }
+    );
+  }, []);
 
   return (
     <NavigationContainer>
       {isSigned ? <AppRoutes /> : <AuthRoutes />}
+      <Toast
+        visible={toastVisible}
+        type="error"
+        message="Seu acesso expirou, faça login novamente"
+      ></Toast>
     </NavigationContainer>
   );
 };
--- src/services/api.js
+++ src/services/api.js
@@ -5,7 +5,8 @@ const api = axios.create({
   // baseURL: API_URL,
   baseURL: 'http://192.168.15.100:3000',
   timeout: 15000,
-  timeoutErrorMessage: 'Isso está demorando mais do que deveria, você pode tentar de novo?',
+  timeoutErrorMessage:
+    'Isso está demorando mais do que deveria, você pode tentar de novo?',
   headers: {
     'Content-Type': 'application/json',
     Accept: 'application/json',
