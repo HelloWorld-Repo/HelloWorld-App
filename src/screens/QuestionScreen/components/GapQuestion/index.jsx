06a50c9e7e2344f51b4b2cf28352b0a451ca8be2
Gabriela Medeiros da Silva <gabriela.medeiros2010@hotmail.com.br>
2022-03-18 16:59:03 -0300

    Fixing gap question styles

--- src/screens/QuestionScreen/components/GapQuestion/index.jsx
+++ src/screens/QuestionScreen/components/GapQuestion/index.jsx
@@ -1,25 +1,18 @@
 import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { Text } from 'react-native-paper';
-import { View } from 'react-native';
+import { Image, View } from 'react-native';
 
 import styles from './style';
 
 const GapQuestion = ({ partsOfDescription }) => {
   return (
     <View style={styles.container}>
-      {partsOfDescription.map((part, index) =>
-        index < partsOfDescription.length - 1 ? (
-          <Fragment key={index}>
-            <Text style={styles.text}>{part}</Text>
-            <View style={styles.gap} />
-          </Fragment>
-        ) : (
-          <Text key={index} style={styles.text}>
-            {part}
-          </Text>
-        )
-      )}
+      <Text style={styles.text}>
+        {partsOfDescription.map((part, index) =>
+          index < partsOfDescription.length - 1 ? `${part}__________` : part
+        )}
+      </Text>
     </View>
   );
 };
